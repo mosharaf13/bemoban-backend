b413af9b56eaba312101fea5e8deba3102485438
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-02-02 00:55:55 +0600

    Add column and card addition feature

--- resources/js/components/Board.vue
+++ resources/js/components/Board.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="bemoban">
         <div class="bemoban__header">
-            <button @click="dumpDatabase()">Dump Database</button>
-            <button>Dump</button>
-            <button>Dump</button>
+            <button @click="addColumn()">Add Column</button>
+            <button @click="addCard()">Add Card</button>
         </div>
         <div class="board">
             <div v-for="column in columns">
@@ -63,10 +62,148 @@ export default {
                     //todo show popup alert
                 })
         },
-        dumpDatabase: function () {
-            axios.post(`${this.apiUrl}/db/dump`)
+
+        addCard: function () {
+            this.$modal.show(
+                {
+                    template: `
+                        <div class="card-details">
+                        <input class="card-details__title" type="text" v-model="title">
+                        <input class="card-details__title" type="text" v-model="description">
+                        <select class="card-details__select" v-model="columnName">
+                          <option disabled value="">Please select a column name</option>
+                          <option v-for="column in columns" v-bind:value="column.id">{{column.title}}</option>
+                        </select>
+
+                        <div class="card-details__btns" v-if="buttons">
+                            <button
+                                v-for="(button, index) in buttons"
+                                :class="button.class || 'vue-dialog-button'"
+                                type="button"
+                                :key="index"
+                                v-html="button.title"
+                                @click.stop="click(index, $event)"
+                            >{{ button.title }}
+                            </button>
+                        </div>
+                        </div>
+                    `,
+                    props: ['buttons', 'columns'],
+                    data: function () {
+                        return {
+                            title: "Please add card title",
+                            description: "Please add card description",
+                            columnName: ''
+                        }
+                    },
+                    methods: {
+                        click(buttonIndex) {
+                            const button = this.buttons[buttonIndex]
+                            const handler = button?.handler
+
+                            if (typeof handler === 'function') {
+                                handler(this.title, this.description, this.columnName)
+                            }
+                        }
+                    }
+                },
+                {
+                    columns: this.columns,
+                    buttons: [
+                        {
+                            title: 'Cancel',
+                            handler: () => {
+                                this.$modal.hideAll();
+                            }
+                        },
+                        {
+                            title: 'Create',
+                            handler: (title, description, columnName) => {
+                                this.submitCard(title, description, columnName);
+                            }
+                        }
+                    ]
+                },
+            )
+        },
+
+        addColumn: function () {
+            this.$modal.show(
+                {
+                    template: `
+                        <div class="card-details">
+                        <input class="card-details__title" type="text" v-model="title">
+
+                        <div class="card-details__btns" v-if="buttons">
+                            <button
+                                v-for="(button, index) in buttons"
+                                :class="button.class || 'vue-dialog-button'"
+                                type="button"
+                                :key="index"
+                                v-html="button.title"
+                                @click.stop="click(index, $event)"
+                            >{{ button.title }}
+                            </button>
+                        </div>
+                        </div>
+                    `,
+                    props: ['buttons'],
+                    data: function () {
+                        return {
+                            title: "Please add column title"
+                        }
+                    },
+                    methods: {
+                        click(buttonIndex) {
+                            const button = this.buttons[buttonIndex]
+                            const handler = button?.handler
+
+                            if (typeof handler === 'function') {
+                                handler(this.title)
+                            }
+                        }
+                    }
+                },
+                {
+                    buttons: [
+                        {
+                            title: 'Cancel',
+                            handler: () => {
+                                this.$modal.hideAll();
+                            }
+                        },
+                        {
+                            title: 'Create',
+                            handler: (title) => {
+                                this.submitColumnTitle(title);
+                            }
+                        }
+                    ]
+                },
+            )
+        },
+
+        submitColumnTitle: function (title) {
+            axios.post(`${this.apiUrl}/columns`, {
+                title: title
+            })
                 .then(response => {
-                    this.columns = response.data
+                    //todo show success alert
+                })
+                .catch(e => {
+                    this.errors.push(e)
+                    //todo show that couldn't fetch columns
+                })
+        },
+
+        submitCard: function (title, description, columnName) {
+            axios.post(`${this.apiUrl}/cards`, {
+                title: title,
+                description: description,
+                column_id: columnName
+            })
+                .then(response => {
+                    //todo show success alert
                 })
                 .catch(e => {
                     this.errors.push(e)
@@ -76,17 +213,20 @@ export default {
     }
 };
 </script>
-
+S
 <style lang="scss">
+@import "../../scss/modal.scss";
 $background-color: #D29034;
 .bemoban {
     background-color: $background-color;
     padding: 2rem;
-    &__header{
+
+    &__header {
         background-color: white;
         padding: 1rem;
         margin: 2rem;
         border-radius: 3px;
+
         button {
             margin: 1rem 2rem;
             padding: .7rem 1rem;

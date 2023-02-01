f185db9a398199815b1a07177807dac928daed5c
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-02-01 13:34:44 +0600

    Add vue

---
+++ resources/js/components/Board.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="board">
+        <column></column>
+    </div>
+</template>
+
+<script>
+import Column from './Column'
+
+export default {
+    components: {
+        Column
+    },
+    data() {
+        return {
+            message: 'Hello world!',
+        };
+    },
+    mounted() {
+        console.log("bla bla");
+    }
+};
+</script>
+
+<style lang="scss">
+$primary: green;
+
+.message {
+    font-size: 18px;
+    font-family: 'Roboto', sans-serif;
+    color: blue;
+    background: $primary;
+}
+</style>
---
+++ resources/js/components/Column.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        This is a column
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello world!',
+        };
+    },
+    mounted() {
+        console.log("bla bla column");
+    }
+};
+</script>
+
+<style lang="scss">
+$primary: green;
+
+.message {
+    font-size: 18px;
+    font-family: 'Roboto', sans-serif;
+    color: blue;
+    background: $primary;
+}
+</style>
